af7b7332ba558803c46d585d0ceffeb7e929c8b6
cone-7 <cone-7@hotmail.com>
2017-01-16 18:21:43 -0600

    Add menu players

---
+++ modules/players/client/config/players.client.menus.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('players')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    menuService.addMenuItem('topbar', {
+      title: 'Jugadores',
+      state: 'players',
+      resolve: {
+        playersResolve: null
+      },
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    menuService.addSubMenuItem('topbar', 'players', {
+      title: 'Registrar',
+      state: 'players.history',
+      roles: ['*']
+    });
+
+    menuService.addSubMenuItem('topbar', 'players', {
+      title: 'Generar Credencial',
+      state: 'players.directiva',
+      roles: ['*']
+    });
+
+  }
+}());
---
+++ modules/players/client/config/players.client.routes.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('players.routes')
+    .config(routeConfig);
+
+  routeConfig.$inject = ['$stateProvider'];
+
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('players', {
+        url: '/players',
+        templateUrl: '/modules/players/client/views/view-players.cliente.view.html',
+        controller: 'PlayersController',
+        controllerAs: 'vm',
+        data: {
+          pageTitle: 'Players'
+        }
+      });
+  }
+
+  // getArticle.$inject = ['$stateParams', 'DirectiveService'];
+
+  // function getArticle($stateParams, ArticlesService) {
+  //   return ArticlesService.get({
+  //     articleId: $stateParams.articleId
+  //   }).$promise;
+  // }
+}());
